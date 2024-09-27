648e2a090e46ea50eec6689e9ee112449108a9f5
asier-acuna <asier.acuna@rai.usc.es>
2024-09-27 15:13:11 +0200

    Agregadas funcionalidades para el rectángulo

---
+++ lib/rectangulo.py
@@ -0,0 +1,8 @@
+def get_area(lado_a: int, lado_b: int) -> int:
+    return lado_a * lado_b
+
+def get_identificador() -> str:
+    return "rectángulo"
+
+def get_perimetro(lado_a: int, lado_b: int) -> int:
+    return 2*lado_a + 2+lado_b
